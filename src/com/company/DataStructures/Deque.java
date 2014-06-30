b3de744cb0d29a11c23696cccf9afd5e8563e80c
GSerjo <sedot8@gmail.com>
2014-06-30 19:23:40 +0400

    * Deque added addLast, addFirst, removeLast, removeFirst

--- src/com/company/DataStructures/Deque.java
+++ src/com/company/DataStructures/Deque.java
@@ -1,5 +1,8 @@
 package com.company.DataStructures;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class Deque<Item>
 {
 
@@ -10,6 +13,7 @@ public class Deque<Item>
     {
         Item value;
         Node next;
+        Node previous;
     }
 
     public int size()
@@ -17,8 +21,114 @@ public class Deque<Item>
         return count;
     }
 
+    private boolean isEmpty()
+    {
+        return first == null && last == null;
+    }
+
     public void addFirst(Item item)
     {
+        if(item == null)
+        {
+            throw new java.lang.NullPointerException();
+        }
+        Node old = first;
+        first = new Node();
+        first.value = item;
+        first.next = old;
+        if(isEmpty())
+        {
+            last = first;
+        }
+        else
+        {
+            old.previous = first;
+        }
+        count++;
+    }
 
+    public void addLast(Item item)
+    {
+        if(item == null)
+        {
+            throw new NullPointerException();
+        }
+        Node old = last;
+        last = new Node();
+        last.value = item;
+        last.previous = old;
+        if(first == null)
+        {
+            first = last;
+        }
+        else
+        {
+            old.next = last;
+        }
+        count++;
+    }
+
+    public Item removeFirst()
+    {
+        if(isEmpty())
+        {
+            throw new java.util.NoSuchElementException();
+        }
+        Item result = first.value;
+        first = first.next;
+        if(first == null)
+        {
+            last = null;
+        }
+        else
+        {
+            first.previous = null;
+        }
+        count--;
+        return result;
+    }
+
+    public Item removeLast()
+    {
+        if(isEmpty())
+        {
+            throw new java.util.NoSuchElementException();
+        }
+        Item result = last.value;
+        last = last.previous;
+        if(last == null)
+        {
+            first = null;
+        }
+        else
+        {
+            last.next = null;
+        }
+        count--;
+        return result;
+    }
+
+    public java.util.Iterator<Item> iterator()
+    {
+        return new DequeIterator();
+    }
+
+    private class DequeIterator implements Iterator<Item>
+    {
+
+        public void remove()
+        {
+            throw new java.lang.UnsupportedOperationException();
+        }
+
+        public boolean hasNext()
+        {
+            return false;
+        }
+
+        public Item next()
+        {
+            return null;
+        }
     }
 }
